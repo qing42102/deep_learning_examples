2a7d0637d0b1189e920be8e1138e070fc46ea257
Qing Zhu <qing42102@outlook.com>
2021-05-19 16:42:06 -0400

    Vectorize calculations over data batches

--- Neural_Network.py
+++ Neural_Network.py
@@ -28,25 +28,23 @@ def load_images(path: str) -> list:
 
     return imgs
 
-train_imgs = load_images("train_data/")
-test_imgs = load_images("test_data/")
+train_dataset = load_images("train_data/")
+test_dataset = load_images("test_data/")
 
 os.chdir("labels/")
 train_label = np.loadtxt("train_label.txt", dtype=int)
 test_label = np.loadtxt("test_label.txt", dtype=int)
 os.chdir("..")
 
-num_features = len(train_imgs[0])
-num_classes = 10
+num_features = len(train_dataset[0])
+num_classes = len(np.unique(test_label))
 
-# Samples per iteration
 batch_size = 50
 
-train_label = tf.one_hot(train_label, depth=num_classes, dtype=tf.float32)
-test_label = tf.one_hot(test_label, depth=num_classes, dtype=tf.float32)
-train_imgs = tf.convert_to_tensor(train_imgs, dtype=tf.float32)
-train_dataset = tf.data.Dataset.from_tensor_slices((train_imgs, train_label)).batch(batch_size)
-test_dataset = tf.convert_to_tensor(test_imgs, dtype=tf.float32)
+train_label = tf.one_hot(train_label, depth=num_classes)
+test_label = tf.one_hot(test_label, depth=num_classes)
+train_dataset = tf.convert_to_tensor(train_dataset, dtype=tf.float32)
+test_dataset = tf.convert_to_tensor(test_dataset, dtype=tf.float32)
 
 print(test_dataset.shape, test_label.shape)
 
@@ -54,8 +52,7 @@ print(test_dataset.shape, test_label.shape)
 def loss(y_output: tf.Tensor, y_label: tf.Tensor) -> float:
     '''
     Cross-entropy loss
-    y_output is a MxK tensor
-    y_label is a MxK tensor
+    y_output and y_label are num_data x num_classes tensors
     '''
 
     total_loss = tf.tensordot(y_label, tf.math.log(y_output), axes=2)
@@ -98,136 +95,91 @@ def digit_accuracy(y_output: tf.Tensor, y_label: tf.Tensor, num_classes: int):
     
     print("\n")
 
-def backward_propagation_output_hidden(y_output: tf.Tensor, y_label: tf.Tensor, W3: np.ndarray, H2: tf.Tensor):
+def backward_propagation_output_hidden(y_output: tf.Tensor, y_label: tf.Tensor, W3: tf.Variable, H2: tf.Tensor):
     '''
     Back propagation from output to hidden layer
-    y_output is a MxK tensor
-    y_label is a MxK tensor
-    W3 is a 100xK tensor
-    H2 is a Mx100 tensor
+    y_output and y_label are num_data x num_classes tensors
+    W3 is a 100 x num_classes tensor
+    H2 is a num_data x 100 tensor
     '''
 
     num_data = y_output.shape[0]
-    num_output = y_output.shape[1]
-    prev_num_output = H2.shape[1]
 
+    # Gradient of the cross-entropy loss
     loss_grad = -tf.divide(y_label, y_output)
 
-    # H2_grad should be Mx100
-    # W3_grad should be 100x10
-    # b3_grad should be 10x1
-    H2_grad = np.zeros((num_data, prev_num_output))
-    W3_grad = tf.zeros((prev_num_output, num_output))
-    b3_grad = tf.zeros((num_output))
+    # z is the forward calculation of the second hidden layer
+    # sigma is the softmax function
+    # Calculate the off diaognals and then the diagonal elements 
+    dsigma_dz = tf.einsum("ij, ik -> ijk", -y_output, y_output)
+    dsigma_dz = tf.linalg.set_diag(dsigma_dz, tf.multiply(y_output, 1-y_output))
 
-    # Sum over each data point
-    for i in range(num_data):
-        # Calculate the off diaognals elements and then the diagonal
-        dsigma_dz = tf.matmul(-y_output[i][:, None], y_output[i][None, :])
-        dsigma_dz = tf.linalg.set_diag(dsigma_dz, y_output[i]*(1-y_output[i]))
+    # \frac{d \sigma(z)}{dz} \nabla y
+    dsigma_dz_loss_grad = tf.einsum("ijk, ik -> ij", dsigma_dz, loss_grad)
 
-        dz_dW3 = np.zeros((prev_num_output, num_output, num_output))
-        # Set the diagonal of the 2nd and 3rd axis
-        dz_dW3[:, np.eye(dz_dW3.shape[1], dtype=bool)] = H2[i][:, None]
-        dz_dW3 = tf.convert_to_tensor(dz_dW3, dtype=tf.float32)
+    H2_grad = tf.matmul(dsigma_dz_loss_grad, tf.transpose(W3))
+    W3_grad = tf.matmul(tf.transpose(H2), dsigma_dz_loss_grad)
+    b3_grad = tf.math.reduce_sum(dsigma_dz_loss_grad, axis=0)
 
-        dsigma_dz_loss_grad = tf.matmul(dsigma_dz, loss_grad[i][:, None])
-        H2_grad[i] = tf.matmul(W3, dsigma_dz_loss_grad).numpy()[:, 0]
-        W3_grad += tf.matmul(dz_dW3, dsigma_dz_loss_grad)[:, :, 0]
-        b3_grad += dsigma_dz_loss_grad[:, 0]
+    # H2_grad should be num_data x 100
+    # W3_grad should be 100 x num_classes
+    # b3_grad should be num_classes
+    return H2_grad, W3_grad/num_data, b3_grad/num_data
 
-    H2_grad = tf.convert_to_tensor(H2_grad, dtype=tf.float32) 
-
-    return H2_grad/num_data, W3_grad/num_data, b3_grad/num_data
-
-def backward_propagation_hidden_hidden(H1: tf.Tensor, H2_grad: tf.Tensor, W2: np.ndarray, b2: np.ndarray):
+def backward_propagation_hidden_hidden(H1: tf.Tensor, H2_grad: tf.Tensor, W2: tf.Variable, b2: tf.Variable):
     '''
     Back propagation from hidden to hidden layer
-    H1 is a Mx100 tensor
-    H2_grad is a 50x100 tensor
+    H1 is a num_data x 100 tensor
+    H2_grad is a num_data x 100 tensor
     W2 is a 100x100 tensor
     b2 is a 100x1 tensor
     '''
 
     num_data = H1.shape[0]
-    num_output = H2_grad.shape[1]
-    prev_num_output = H1.shape[1]
 
+    # The forward calculation of the first hidden layer
     z = tf.matmul(H1, W2) + tf.transpose(b2[:, None])
 
-    # H1_grad should be Mx100
+    # \frac{d \phi(z)}{dz} \nabla H_2
+    # phi is the activation function
+    dphi_dz_H2_grad = tf.multiply(tf.cast(z>0, tf.float32), H2_grad)
+
+    H1_grad = tf.matmul(dphi_dz_H2_grad, tf.transpose(W2))
+    W2_grad = tf.matmul(tf.transpose(H1), dphi_dz_H2_grad)
+    b2_grad = tf.math.reduce_sum(dphi_dz_H2_grad, axis=0)
+
+    # H1_grad should be num_data x 100
     # W2_grad should be 100x100
     # b2_grad should be 100x1
-    H1_grad = np.zeros((num_data, prev_num_output))
-    W2_grad = tf.zeros((prev_num_output, num_output))
-    b2_grad = tf.zeros((num_output))
+    return H1_grad, W2_grad/num_data, b2_grad/num_data
 
-    # Sum over each data point
-    for i in range(num_data):
-        dphi_dz = tf.zeros((num_output, num_output))
-        # Set the diagonal based on the sign of z[i]
-        dphi_dz = tf.linalg.set_diag(dphi_dz, tf.cast(z[i]>0, tf.float32))
-
-        dz_dW2 = np.zeros((prev_num_output, num_output, num_output))
-        # Set the diagonal of the 2nd and 3rd axis
-        dz_dW2[:, np.eye(dz_dW2.shape[1], dtype=bool)] = H1[i][:, None]
-        dz_dW2 = tf.convert_to_tensor(dz_dW2, dtype=tf.float32)
-
-        dphi_dz_H2_grad = tf.matmul(dphi_dz, H2_grad[i][:, None])
-        H1_grad[i] = tf.matmul(W2, dphi_dz_H2_grad).numpy()[:, 0]
-        W2_grad += tf.matmul(dz_dW2, dphi_dz_H2_grad)[:, :, 0]
-        b2_grad += dphi_dz_H2_grad[:, 0]
-
-    H1_grad = tf.convert_to_tensor(H1_grad, dtype=tf.float32)
-
-    return H1_grad/num_data, W2_grad/num_data, b2_grad/num_data
-
-def backward_propagation_hidden_input(X: tf.Tensor, H1_grad: tf.Tensor, W1: np.ndarray, b1: np.ndarray):
+def backward_propagation_hidden_input(X: tf.Tensor, H1_grad: tf.Tensor, W1: tf.Variable, b1: tf.Variable):
     '''
     Back propagation from hidden to input layer
-    X is a Mx784 tensor
-    H1_grad is a 50x100 tensor
-    W1 is a 784x100 tensor
+    X is a num_data x num_features tensor
+    H1_grad is a num_data x 100 tensor
+    W1 is a num_features x 100 tensor
     b1 is a 100x1 tensor
     '''
-    
-    num_data = X.shape[0]
-    num_output = H1_grad.shape[1]
-    prev_num_output = X.shape[1]
 
+    num_data = X.shape[0]
+
+    # The forward calculation of the input layer
     z = tf.matmul(X, W1) + tf.transpose(b1[:, None])
 
-    # W1_grad should be 784x100
+    # \frac{d \phi(z)}{dz} \nabla H_1
+    # phi is the activation function
+    dphi_dz_H1_grad = tf.multiply(tf.cast(z>0, tf.float32), H1_grad)
+
+    W1_grad = tf.matmul(tf.transpose(X), dphi_dz_H1_grad)
+    b1_grad = tf.math.reduce_sum(dphi_dz_H1_grad, axis=0)
+
+    # W1_grad should be num_features x 100
     # b1_grad should be 100x1
-    W1_grad = tf.zeros((prev_num_output, num_output))
-    b1_grad = tf.zeros((num_output))
-
-    # Sum over each data point
-    for i in range(num_data):
-        dphi_dz = tf.zeros((num_output, num_output))
-        # Set the diagonal based on the sign of z[i]
-        dphi_dz = tf.linalg.set_diag(dphi_dz, tf.cast(z[i]>0, tf.float32))
-
-        dz_dW1 = np.zeros((prev_num_output, num_output, num_output))
-        # Set the diagonal of the 2nd and 3rd axis
-        dz_dW1[:, np.eye(dz_dW1.shape[1], dtype=bool)] = X[i][:, None]
-        dz_dW1 = tf.convert_to_tensor(dz_dW1, dtype=tf.float32)
-
-        dphi_dz_H1_grad = tf.matmul(dphi_dz, H1_grad[i][:, None])
-        W1_grad += tf.matmul(dz_dW1, dphi_dz_H1_grad)[:, :, 0]
-        b1_grad += dphi_dz_H1_grad[:, 0]
-
     return W1_grad/num_data, b1_grad/num_data
 
-def neural_network_model(train_dataset, test_dataset, test_label):
-    '''
-    X is a MxN tensor
-    y is a MxK tensor
-    '''
-
-    # Get the entire training dataset as one tensor
-    entire_train_x = tf.concat([x for x, y in train_dataset], axis=0)
-    entire_train_y = tf.concat([y for x, y in train_dataset], axis=0)
+def neural_network_model(train_dataset, test_dataset, train_label, test_label):
+    train_batches = tf.data.Dataset.from_tensor_slices((train_dataset, train_label)).batch(batch_size)
 
     # Neural network layers for forward propagation
     hidden1 = tf.keras.layers.Dense(100, activation = 'relu')
@@ -240,18 +192,15 @@ def neural_network_model(train_dataset, test_dataset, test_label):
     testing_loss_list = []
 
     epoch = 0
-    while epoch < 1:
+    while epoch < 10:
         epoch += 1
+        print(epoch)
 
-        stepsize = 1/epoch
+        stepsize = 0.1*1/epoch
         optimizer = tf.keras.optimizers.SGD(learning_rate=stepsize)
 
-        iteration = 0
-        for x, y in train_dataset:
-            iteration += 1
-            print(iteration)
-            
-            # Forward propagation
+        start = time.time()
+        for x, y in train_batches:
             H1 = hidden1(x)
             H2 = hidden2(H1)
             y_output = output(H2)
@@ -263,32 +212,33 @@ def neural_network_model(train_dataset, test_dataset, test_label):
             W3 = output.trainable_weights[0]
             b3 = output.trainable_weights[1]
             
-            start = time.time()
             H2_grad, W3_grad, b3_grad = backward_propagation_output_hidden(y_output, y, W3, H2)
             H1_grad, W2_grad, b2_grad = backward_propagation_hidden_hidden(H1, H2_grad, W2, b2)
             W1_grad, b1_grad = backward_propagation_hidden_input(x, H1_grad, W1, b1)
-            print(time.time()-start)
-            
+
             # Apply the gradient to each parameter
-            optimizer.apply_gradients(zip([W1_grad, b1_grad, W2_grad, b2_grad, W3_grad, b3_grad], [W1, b1, W2, b2, W3, b3]))
+            optimizer.apply_gradients(zip([W1_grad, b1_grad, W2_grad, b2_grad, W3_grad, b3_grad], 
+                                            [W1, b1, W2, b2, W3, b3]))
+        
+        print(time.time()-start)
 
-            if iteration % 10 == 0:
-                train_output = output(hidden2(hidden1(entire_train_x)))
-                training_accuracy = classification_accuracy(train_output, entire_train_y)
-                training_loss = loss(train_output, entire_train_y)
+        train_output = output(hidden2(hidden1(train_dataset)))
+        training_accuracy = classification_accuracy(train_output, train_label)
+        training_loss = loss(train_output, train_label)
 
-                test_output = output(hidden2(hidden1(test_dataset)))
-                testing_accuracy = classification_accuracy(test_output, test_label)
-                testing_loss = loss(test_output, test_label)
-                
-                training_accuracy_list.append(training_accuracy)
-                testing_accuracy_list.append(testing_accuracy)
-                training_loss_list.append(training_loss)
-                testing_loss_list.append(testing_loss)
+        test_output = output(hidden2(hidden1(test_dataset)))
+        testing_accuracy = classification_accuracy(test_output, test_label)
+        testing_loss = loss(test_output, test_label)
+        
+        training_accuracy_list.append(training_accuracy)
+        testing_accuracy_list.append(testing_accuracy)
+        training_loss_list.append(training_loss)
+        testing_loss_list.append(testing_loss)
 
-                print("Training Accuracy:", training_accuracy, "Training Loss:", training_loss)
-                print("Testing Accuracy:", testing_accuracy, "Testing Loss:", testing_loss)
+        print("Training Accuracy:", training_accuracy, "Training Loss:", training_loss)
+        print("Testing Accuracy:", testing_accuracy, "Testing Loss:", testing_loss)
 
+    print("\n")
     digit_accuracy(test_output, test_label, num_classes)
 
     W_optimal = [W1.numpy(), b1.numpy(), W2.numpy(), b2.numpy(), W3.numpy(), b3.numpy()]
@@ -296,25 +246,24 @@ def neural_network_model(train_dataset, test_dataset, test_label):
     return W_optimal, training_accuracy_list, testing_accuracy_list, training_loss_list, testing_loss_list
 
 # %%
-
 tf.random.set_seed(0)
 
-results = neural_network_model(train_dataset, test_dataset, test_label)
+results = neural_network_model(train_dataset, test_dataset, train_label, test_label)
 W_optimal, training_accuracy, testing_accuracy, training_loss, testing_loss = results
 
-iteration = np.arange(len(training_accuracy)*10, step=10)
+epoch = np.arange(len(training_accuracy))
 
 plt.figure(figsize=(8, 5))
-plt.plot(iteration, training_accuracy, label="Training accuracy")
-plt.plot(iteration, testing_accuracy, label="Testing accuracy")
+plt.plot(epoch, training_accuracy, label="Training accuracy")
+plt.plot(epoch, testing_accuracy, label="Testing accuracy")
 plt.xlabel("Iteration", fontsize=14)
 plt.ylabel("Percentage", fontsize=14)
 plt.legend()
 plt.show()
 
 plt.figure(figsize=(8, 5))
-plt.plot(iteration, training_loss, label="Training loss")
-plt.plot(iteration, testing_loss, label="Testing loss")
+plt.plot(epoch, training_loss, label="Training loss")
+plt.plot(epoch, testing_loss, label="Testing loss")
 plt.xlabel("Iteration", fontsize=14)
 plt.ylabel("Loss", fontsize=14)
 plt.legend()
@@ -323,3 +272,5 @@ plt.show()
 filehandler = open("nn_parameters.txt","wb") 
 pickle.dump(W_optimal, filehandler, protocol=2) 
 filehandler.close() 
+
+# %%
